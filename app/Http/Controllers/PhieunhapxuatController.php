488feccb76868fe7844c8bbf118c2951fdfa9c89
DanKT15 <105506442+DanKT15@users.noreply.github.com>
2023-10-24 17:54:55 +0700

    24/10/2023

--- app/Http/Controllers/PhieunhapxuatController.php
+++ app/Http/Controllers/PhieunhapxuatController.php
@@ -19,7 +19,13 @@ use App\Models\Tonkho;
 class PhieunhapxuatController extends Controller
 {
     public function index(){   // Giao diện hiển thị toàn bộ dữ liệu: GET
-        return view("giaodien.app", ['page' => "phieunhapxuat.DSphieu"]);
+
+        $phieu = Phieunhapxuat::all();
+
+        return view("giaodien.app", [
+            'page' => "phieunhapxuat.DSphieu",
+            'phieu' => $phieu
+        ]);
     }
 
     public function create(){   // Giao diện thêm dữ liệu: GET
@@ -68,14 +74,96 @@ class PhieunhapxuatController extends Controller
 
         $request->validate($rules, $mess);
 
-        $sophieu = $request->sophieu;
-
-        $idnhanvien = Auth::id();
-
-        $nhanvien =  Nhansu::firstWhere('MANV', $idnhanvien);
-
         try {
 
+            $sophieu = $request->sophieu;
+
+            $idnhanvien = Auth::id();
+
+            $nhanvien =  Nhansu::firstWhere('MANV', $idnhanvien);
+
+            $Trangthai = Trangthai::find($request->matrangthai);
+
+            $makho = $nhanvien->MAKHO;
+
+            // tạo or cập nhật tồn kho
+            switch ($Trangthai->TENTT) {
+
+                case 'Xuất':
+                    
+                    foreach ($request->sp as $key => $value) {
+
+                        $tonkho = Tonkho::where('MAKHO', $makho)
+                        ->where('MASP', $value['idsp'])
+                        ->first();
+
+                        if (empty($tonkho)) {
+
+                            $sp = Sanpham::find($value['idsp']);
+
+                            Tonkho::create([
+                                'MAKHO' => $makho,
+                                'MASP' => $value['idsp'],
+                                'SLTONKHO' => 0,
+                                'SLNHAP' => 0,
+                                'SLXUAT' => 0
+                            ]);
+
+                            return back()->with('err', 'Hiện chưa có sản phẩm '.$sp->TENSP.' trong kho, vui lòng nhập hàng');
+                        }
+                        else {
+
+                            if ($tonkho->SLTONKHO < $value['slsp'] || $value['slsp'] <= 0) {
+
+                                $sp = Sanpham::find($value['idsp']);
+    
+                                return back()->with('err', 'Hiện chỉ còn '.$tonkho->SLTONKHO.' sản phẩm '.$sp->TENSP);
+                            }
+    
+                            Tonkho::where('MAKHO', $makho)
+                            ->where('MASP', $value['idsp'])
+                            ->update([
+                                'SLTONKHO' => $tonkho->SLTONKHO - $value['slsp'],
+                                'SLXUAT' => $tonkho->SLXUAT + $value['slsp'],
+                            ]);
+                        }
+
+                    }
+
+                    break;
+                
+                default:
+                    
+                    foreach ($request->sp as $key => $value) {
+
+                        $tonkho = Tonkho::where('MAKHO', $makho)
+                        ->where('MASP', $value['idsp'])
+                        ->first();
+
+                        if (empty($tonkho)) {
+                            Tonkho::create([
+                                'MAKHO' => $makho,
+                                'MASP' => $value['idsp'],
+                                'SLTONKHO' => $tonkho->SLTONKHO + $value['slsp'],
+                                'SLNHAP' => $tonkho->SLNHAP + $value['slsp'],
+                                'SLXUAT' => 0
+                            ]);
+                        }
+                        else {
+                            Tonkho::where('MAKHO', $makho)
+                            ->where('MASP', $value['idsp'])
+                            ->update([
+                                'SLTONKHO' => $tonkho->SLTONKHO + $value['slsp'],
+                                'SLNHAP' => $tonkho->SLNHAP + $value['slsp'],
+                            ]);
+                        }
+
+                    }
+
+                    break;
+            }
+
+            // tạo phiếu và chi tiết phiếu
             $idphieu = Phieunhapxuat::insertGetId([
                 'SOPHIEU' => $request->sophieu,
                 'MANV' => $idnhanvien,
@@ -92,7 +180,7 @@ class PhieunhapxuatController extends Controller
                 $gia = $sp->GIASP;
                 $dongia = $gia * $sl;
                 
-                $CTnhapxuat = CTnhapxuat::create([
+                CTnhapxuat::create([
                     'SOLUONG' => $sl, 
                     'DONGIA' => $gia, 
                     'THANHTIEN' => $dongia, 
@@ -102,10 +190,70 @@ class PhieunhapxuatController extends Controller
 
             }
 
+            return back()->with('alert', 'Thêm dữ liệu thành công');
+
+        } catch (Exception $err) {
+            return back()->withError($err->getMessage())->withInput();
+        }
+        
+    }
+
+    public function show($id){   // Lấy chi tiết của một dữ liệu: GET
+        $phieu = Phieunhapxuat::find($id);
+
+        return view("giaodien.app", [
+            'page' => "phieunhapxuat.DSphieu",
+            'phieu' => $phieu
+        ]);
+    }
+
+    public function edit($id){   // Giao diện cập nhật dữ liệu: GET
+
+        $phieu = Phieunhapxuat::find($id);
+
+        return view("giaodien.app", [
+            'page' => "phieunhapxuat.ViewUpdataPhieu",
+            'phieu' => $phieu
+        ]);
+    }
+
+    public function update(Request $request, $id){   // Cập nhật lại dữ liệu: POST
+        
+        $rules = [
+            'sophieu' => 'required|alpha|max:20',
+            'madiachi' => 'required|numeric',
+            'matrangthai' => 'required|numeric',
+            'sp.*.slsp' => 'required|numeric',
+            'sp.*.idsp' => 'required|numeric|distinct:strict'
+        ];
+
+        $mess = [
+            'sophieu.required' => 'Chưa nhập thông tin',
+            'sophieu.alpha' => 'Vui lòng nhập ký tự chữ cái',
+            'sophieu.max' => 'Tối đa 20 kí tự',
+            'madiachi.required' => 'Chưa chọn thông tin',
+            'matrangthai.required' => 'Chưa chọn thông tin',
+            'sp.*.idsp.required' => 'Chưa chọn thông tin',
+            'sp.*.idsp.distinct' => 'Không được trùng lặp thông tin',
+            'sp.*.slsp.required' => 'Vui lòng nhập số lượng',
+            'sp.*.slsp.numeric' => 'Vui lòng nhập chữ số',
+        ];
+
+        $request->validate($rules, $mess);
+
+        try {
+
+            $sophieu = $request->sophieu;
+
+            $idnhanvien = Auth::id();
+
+            $nhanvien =  Nhansu::firstWhere('MANV', $idnhanvien);
+
             $Trangthai = Trangthai::find($request->matrangthai);
 
             $makho = $nhanvien->MAKHO;
 
+            // cập nhật tồn kho
             switch ($Trangthai->TENTT) {
 
                 case 'Xuất':
@@ -116,19 +264,36 @@ class PhieunhapxuatController extends Controller
                         ->where('MASP', $value['idsp'])
                         ->first();
 
-                        if ($tonkho->SLTONKHO < $value['slsp'] || $value['slsp'] <= 0) {
+                        if (empty($tonkho)) {
 
                             $sp = Sanpham::find($value['idsp']);
 
-                            return back()->with('err', 'Hiện chỉ còn '.$tonkho->SLTONKHO.' sản phẩm '.$sp->TENSP);
-                        }
+                            Tonkho::create([
+                                'MAKHO' => $makho,
+                                'MASP' => $value['idsp'],
+                                'SLTONKHO' => 0,
+                                'SLNHAP' => 0,
+                                'SLXUAT' => 0
+                            ]);
 
-                        Tonkho::where('MAKHO', $makho)
-                        ->where('MASP', $value['idsp'])
-                        ->update([
-                            'SLTONKHO' => $tonkho->SLTONKHO - $value['slsp'],
-                            'SLXUAT' => $tonkho->SLXUAT + $value['slsp'],
-                        ]);
+                            return back()->with('err', 'Hiện chưa có sản phẩm '.$sp->TENSP.' trong kho, vui lòng nhập hàng');
+                        }
+                        else {
+
+                            if ($tonkho->SLTONKHO < $value['slsp'] || $value['slsp'] <= 0) {
+
+                                $sp = Sanpham::find($value['idsp']);
+    
+                                return back()->with('err', 'Hiện chỉ còn '.$tonkho->SLTONKHO.' sản phẩm '.$sp->TENSP);
+                            }
+    
+                            Tonkho::where('MAKHO', $makho)
+                            ->where('MASP', $value['idsp'])
+                            ->update([
+                                'SLTONKHO' => $tonkho->SLTONKHO - $value['slsp'],
+                                'SLXUAT' => $tonkho->SLXUAT + $value['slsp'],
+                            ]);
+                        }
 
                     }
 
@@ -142,39 +307,67 @@ class PhieunhapxuatController extends Controller
                         ->where('MASP', $value['idsp'])
                         ->first();
 
-                        Tonkho::where('MAKHO', $makho)
-                        ->where('MASP', $value['idsp'])
-                        ->update([
-                            'SLTONKHO' => $tonkho->SLTONKHO + $value['slsp'],
-                            'SLNHAP' => $tonkho->SLNHAP + $value['slsp'],
-                        ]);
+                        if (empty($tonkho)) {
+                            Tonkho::create([
+                                'MAKHO' => $makho,
+                                'MASP' => $value['idsp'],
+                                'SLTONKHO' => $tonkho->SLTONKHO + $value['slsp'],
+                                'SLNHAP' => $tonkho->SLNHAP + $value['slsp'],
+                                'SLXUAT' => 0
+                            ]);
+                        }
+                        else {
+                            Tonkho::where('MAKHO', $makho)
+                            ->where('MASP', $value['idsp'])
+                            ->update([
+                                'SLTONKHO' => $tonkho->SLTONKHO + $value['slsp'],
+                                'SLNHAP' => $tonkho->SLNHAP + $value['slsp'],
+                            ]);
+                        }
 
                     }
 
                     break;
             }
 
-            return back()->with('alert', 'Thêm dữ liệu thành công');
+            // cập nhật phiếu và chi tiết phiếu
+            Phieunhapxuat::where('id', $id)
+                            ->update([
+                                'SOPHIEU' => $request->sophieu,
+                                'MADC' => $request->madiachi,
+                                'MATT' => $request->matrangthai
+                            ]);
+    
+            Phieunhapxuat::where('id', $id)->update(['SOPHIEU' => strtoupper($sophieu).'-'.$id]);
+
+            foreach ($request->sp as $key => $value) {
+
+                $sl = $value['slsp'];
+                $sp = Sanpham::find($value['idsp']);
+                $gia = $sp->GIASP;
+                $dongia = $gia * $sl;
+
+                CTnhapxuat::where('MAPHIEU', $id)
+                            ->where('MASP', $value['idsp'])
+                            ->update([
+                                'SOLUONG' => $sl, 
+                                'DONGIA' => $gia, 
+                                'THANHTIEN' => $dongia, 
+                                'MAPHIEU' => $id, 
+                            ]);
+
+            }
+
+            return back()->with('alert', 'Cập nhật thành công');
 
         } catch (Exception $err) {
             return back()->withError($err->getMessage())->withInput();
         }
-        
-    }
 
-    public function show($id){   // Lấy chi tiết của một dữ liệu: GET
-        
-    }
-
-    public function edit($id){   // Giao diện cập nhật dữ liệu: GET
-        
-    }
-
-    public function update(Request $request, $id){   // Cập nhật lại dữ liệu: POST
-        
     }
 
     public function destroy($id){   // Xóa bỏ một dữ liệu: GET
-        
+        $phieu = Phieunhapxuat::find($id);
+        $phieu->delete();
     }
 }
